ad0b9067fdb702e8fde2c6336bd11175f1f8f642
Iassirskyi <s.iassirskyi@gmail.com>
2021-02-02 14:42:15 +0200

    updates for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn topic_project.wsgi --log-file -
--- topic_project/settings.py
+++ topic_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'kng!v@6uu%bl=&^56b$w5xcszky-ib1*ng8)itp^4_wd!1gon8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
