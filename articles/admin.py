4287381dabf8c80533edc0f76a66ae299afeb24f
Iassirskyi <s.iassirskyi@gmail.com>
2021-04-07 11:46:45 +0300

    added entries

--- articles/admin.py
+++ articles/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Article, Entry
 
 admin.site.register(Article)
-admin.site.register(Entry)
+#admin.site.register(Entry)
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ['title']
